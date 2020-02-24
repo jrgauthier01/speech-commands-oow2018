f3e792a787421ba31ff7e7a448e767ee47bfaa37
jrgauthier01 <jrgauthier01@gmail.com>
2020-02-24 22:25:47 +0000

    deleting old files

--- notebooks/get-data-from-oci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-data_dir="/home/datascience/data"
-mkdir ${data_dir} && cd ${data_dir} 
-
-wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/x-h8WMZtbxt5X1hkych796-m5GuN_fg_HANhVw-S5IY/n/paasdevdatasc/b/oow-2018/o/data_speech_commands_v0.02.tar.gz
-
-gunzip data_speech_commands_v0.02.tar.gz && tar -xvf data_speech_commands_v0.02.tar
